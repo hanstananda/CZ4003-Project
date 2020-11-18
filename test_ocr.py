6398da7c9e9057ebe82cbfb819a3d90da3b27cf2
hanstananda <hanstananda@gmail.com>
2020-11-18 23:00:43 +0800

    added tresholding methods testing

--- test_ocr.py
+++ test_ocr.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+import cv2 as cv
+import numpy
 from PIL import Image
 import pytesseract
 import difflib
@@ -10,6 +12,33 @@ text_folder = "./source"
 images = ["sample01.png", "sample02.png"]
 texts = ["sample01.txt", "sample02.txt"]
 
+
+def otsu_tresholding(image_pil):
+    img_cv = cv.cvtColor(numpy.array(image_pil), cv.COLOR_RGB2GRAY)
+    # Otsu's thresholding
+    ret, th = cv.threshold(img_cv, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    img_th_rgb = cv.cvtColor(th, cv.COLOR_GRAY2RGB)
+    img_th_pil = Image.fromarray(img_th_rgb)
+    # img_th_pil.show()
+    return img_th_pil
+
+
+def adaptive_gaussian_tresholding(image_pil):
+    img_cv = cv.cvtColor(numpy.array(image_pil), cv.COLOR_RGB2GRAY)
+    th = cv.adaptiveThreshold(img_cv, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                              cv.THRESH_BINARY, 11, 2)
+    img_th_rgb = cv.cvtColor(th, cv.COLOR_GRAY2RGB)
+    img_th_pil = Image.fromarray(img_th_rgb)
+    # img_th_pil.show()
+    return img_th_pil
+
+
+def evaluate(actual, expected):
+    s = difflib.SequenceMatcher(None, actual, expected)
+    print(s.ratio())
+    print(s.get_matching_blocks())
+
+
 for idx, image_name in enumerate(images):
     image = Image.open(os.path.join(image_folder, image_name))
     print(image.format, image.mode)
@@ -17,15 +46,24 @@ for idx, image_name in enumerate(images):
         image = image.convert("RGB")
     result = pytesseract.image_to_string(image)
     # result = result.split("\n")
-    pprint(result)
+    # pprint(result)
 
     with open(os.path.join(text_folder, texts[idx]), 'r') as f:
         base_text = f.readlines()
         base_text = "".join(base_text)
         # base_text = [line.strip() for line in base_text]
 
-    pprint(base_text)
-    s = difflib.SequenceMatcher(None, result, base_text)
-    print(s.ratio())
-    print(s.get_matching_blocks())
+    evaluate(result, base_text)
 
+    image_th = otsu_tresholding(image)
+    result_th = pytesseract.image_to_string(image_th)
+
+    evaluate(result_th, base_text)
+
+    image_th_adaptive_gaussian = adaptive_gaussian_tresholding(image)
+    image_th_adaptive_gaussian = otsu_tresholding(image_th_adaptive_gaussian)
+    result_th_adaptive_gaussian = pytesseract.image_to_string(image_th_adaptive_gaussian)
+    print(result_th_adaptive_gaussian)
+    evaluate(result_th_adaptive_gaussian, base_text)
+
+    break
